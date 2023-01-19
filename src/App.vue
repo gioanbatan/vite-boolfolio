86c77ac4f30ee318f9695296d80d83471909fe9d
gioanbatan <giovannibassani@hotmail.com>
2023-01-19 17:11:21 +0100

    Add: AppMain, show projects in console.log and page (milestone4)

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,19 @@
 <script>
-import axios from 'axios';
+import AppMain from './components/AppMain.vue';
 
 export default {
   name: 'App',
   components: {
-
-  },
-  data() {
-    return {
-      test: 'ciao',
-      projects: [],
-    }
-  },
-  methods: {
-    getProjects() {
-      console.log("PRIMA", this.projects);
-
-      axios.get(`http://127.0.0.1:8000/api/projects`).then(resp => {
-        this.projects = resp.data.results;
-        console.log("RISPOSTA", resp.data.results);
-      });
-    }
-  },
-  created() {
-    this.getProjects();
-    console.log('created va!');
+    AppMain
   }
 }
-
 </script>
 
 
 <template>
-
-  {{ test }}
+  <main>
+    <AppMain />
+  </main>
 </template>
 
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'AppMain',
+    components: {
+
+    },
+    data() {
+        return {
+            projects: [],
+        }
+    },
+    created() {
+        this.getProjects();
+    },
+    methods: {
+        getProjects() {
+            console.log("PRIMA", this.projects);
+
+            axios.get(`http://127.0.0.1:8000/api/projects`).then(resp => {
+                this.projects = resp.data.results;
+                console.log("RISPOSTA", resp.data.results);
+            });
+        }
+    },
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="project in projects">{{ project.title }}</li>
+    </ul>
+</template>
+
+<style lang="scss">
+@use "../styles/general.scss" as *;
+</style>
